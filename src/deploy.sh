a87e93aac5b35ae1ce24fc11c88aad1c49e62817
Jakkwj <shutupwj@163.com>
2025-01-01 09:20:17 +0800

    updated

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 自动部署到 github page
-git config --global http.proxy 'http://127.0.0.1:10809'  # 设置 git 代理
-git config --global https.proxy 'http://127.0.0.1:10809'
+git config --global http.proxy 'http://127.0.0.1:37799'  # 设置 git 代理
+git config --global https.proxy 'http://127.0.0.1:37799'
 yarn config set proxy "http://127.0.0.1:37799"  # 设置 yarn 代理
 yarn config set https-proxy "http://127.0.0.1:37799"
 
 git add -A && git commit -m "updated" && git push -u origin master
 
-# USE_SSH=true yarn deploy  # 部署到 github page
+USE_SSH=true yarn deploy  # 部署到 github page
 
 git config --global --unset http.proxy  # 取消 git 代理
 git config --global --unset https.proxy
