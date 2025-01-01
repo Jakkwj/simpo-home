3107c5e752081f91ac96ce815c57591553181d6d
Jakkwj <shutupwj@163.com>
2025-01-01 10:17:23 +0800

    update

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ "$1" == "proxy" ]; then  # ./deploy.sh proxy 运行时
-    proxy_ip=http://127.0.0.1:37799
-    git config --global http.proxy '$proxy_ip'  # 设置 git 代理
-    git config --global https.proxy '$proxy_ip'
-    yarn config set proxy "$proxy_ip"  # 设置 yarn 代理
-    yarn config set https-proxy "$proxy_ip"
+    proxy_ip='http://127.0.0.1:37799'
+    git config --global http.proxy $proxy_ip  # 设置 git 代理
+    git config --global https.proxy $proxy_ip
+    yarn config set proxy $proxy_ip  # 设置 yarn 代理
+    yarn config set https-proxy $proxy_ip
 fi
 
-git add -A && git commit -m "updated" && git push -u origin master
+# git add -A && git commit -m "updated" && git push -u origin master
+git push -u origin master
 
-USE_SSH=true yarn deploy  # 自动部署到 github page
+# USE_SSH=true yarn deploy  # 自动部署到 github page
 
 if [ "$1" == "proxy" ]; then
     git config --global --unset http.proxy  # 取消 git 代理
